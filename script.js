a1627a2e192239bd1ed648889c53b6a5c0e2dae6
West-Tiwan <xyzmohit.singh@gmail.com>
2023-09-06 11:05:06 +0530

    modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -13,5 +13,6 @@ http.createServer(function (req, res) {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
         res.end();
-    })
+    })         
 }).listen(3000);
+ 
