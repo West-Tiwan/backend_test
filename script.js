7b0e1841db0941ca6d7b29a05d600de8a346a686
harry <xyzmohit.singh@gmail.com>
2023-10-23 22:43:08 +0530

    async reading

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
 fs.writeFileSync("bio.txt","test from thapa technical ");
 fs.appendFileSync("bio.txt","this is in video 6");
-const readed = fs.readFileSync("bio.txt");
-console.log(readed.toString());
+const readed = fs.readFile("bio.txt","UTF-8",(err,data)=>{
+    console.log(data);
+});
 fs.renameSync("bio.txt","mybio.txt");
 fs.rmSync("mybio.txt");
