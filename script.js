bcaddbd7b7255e3481337d0c51e510bf70b5611f
harry <xyzmohit.singh@gmail.com>
2023-10-23 22:32:35 +0530

    modified:   script.js

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-
-http.createServer(function (req, res) {
-    var q = url.parse(req.url, true);
-    var filename = `./${q.pathname}`;
-    fs.readFile(filename, function (err, data) {
-        if (err) {
-            res.writeHead(404, { 'Content-Type': 'text/html' })
-            return res.end("404 Not found");
-        }
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        res.end();
-    })         
-}).listen(3000);
- 
+const fs = require("fs");
+fs.writeFileSync("bio.txt","test from thapa technical ");
+fs.appendFileSync("bio.txt","this is in video 6");
+const readed = fs.readFileSync("bio.txt");
+console.log(readed.toString());
+fs.renameSync("bio.txt","mybio.txt");
