f055d9f6d6a41d51fe92b079dff0ac68e9253c3a
harry <xyzmohit.singh@gmail.com>
2023-11-04 15:18:46 +0530

    routing

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const http = require("http");
 const url = require("url");
 const server = http.createServer((req,res)=>{
-    console.log(req.url);
-    res.end("hello from the other side");
+    if(req.url=="/"){
+        res.end("hello from /");
+    }
+    else if(req.url=="/about"){
+        res.end("hello from /about");
+    }
+    else if(req.url=="/contact"){
+        res.end("hello from /contact");
+    }
+    else{
+        res.writeHead(404,{"Content-type":"text/html"})
+        res.end("<h1>404 error page</h1>");
+    }
 });
 server.listen(3000,"127.0.0.1",()=>{
     console.log("listening on port 3000");
