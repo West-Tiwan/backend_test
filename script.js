1e3f4475076032092832a5aa6a7334c4809c10e1
harry <xyzmohit.singh@gmail.com>
2023-11-04 15:13:03 +0530

    req http

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const http = require("http");
+const url = require("url");
 const server = http.createServer((req,res)=>{
+    console.log(req.url);
     res.end("hello from the other side");
 });
 server.listen(3000,"127.0.0.1",()=>{
