3ed1f1724a3a84f9e44e1d105cb0c7835127f1de
harry <xyzmohit.singh@gmail.com>
2023-11-04 16:43:04 +0530

    rstream.pipe

--- script.js
+++ script.js
@@ -4,12 +4,7 @@ const server = http.createServer();
 server.on('request', (req, res) => {
     fs.readFile('input.txt', function (err, data) {
         const rstream = fs.createReadStream("input.txt");
-        rstream.on('data', (chunkdata) => {
-            res.write(chunkdata);
-        });
-        rstream.on('end',()=>{
-            res.end();
-        });
+        rstream.pipe(res);
     })});
 server.listen(3000, "127.0.0.1", () => {
     console.log("listening on port 3000");
