159f6dce80824ba47a4b8c520282f351d71b142c
harry <xyzmohit.singh@gmail.com>
2023-11-04 16:15:20 +0530

    events

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const http = require("http");
 const url = require("url");
 const fs = require("fs");
-var jsony;
+const EventEmitter = require("events");
+const event = new EventEmitter();
+event.on("yoyo",()=>{
+    console.log(event);
+})
+event.on("yoyo",(sc,msg)=>{
+    console.log(`status code: ${sc} and the page is: ${msg}`);
+})
+event.emit("yoyo",200,"borked"); 
 const server = http.createServer((req, res) => {
     fs.readFile(`${__dirname}/user.json`, "utf-8", (err, data) => {
         console.log(data);
-        jsony = JSON.parse(data);
+        var jsony = JSON.parse(data);
         if (req.url == "/") {
             res.end("hello from /");
         }
@@ -16,6 +24,7 @@ const server = http.createServer((req, res) => {
             res.end("hello from /contact");
         }
         else if (req.url == "/api") {
+            res.writeHead(200,{"content-type":"text/html"});
             res.end(jsony.car);
         }
         else {
