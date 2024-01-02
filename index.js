e3545e9563d4e50cbf299ac3f70ef8a69e2973b8
harry <xyzmohit.singh@gmail.com>
2024-01-02 12:25:01 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
+const port = 3000;
 
 app.get('/', function (req, res) {
-    res.sendFile('index.html');
+    res.sendFile('summer.html');
 });
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
