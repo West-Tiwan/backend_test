80b1d646e5deb0f02a0dbd1313159420761390f9
West-Tiwan <xyzmohit.singh@gmail.com>
2024-03-26 21:20:49 +0530

    form

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 import express from "express";
-const server = express();
+import path from "path";
 
-server.get("/",(req,res)=>{
-    res.render("index.html")
-})
-server.listen("3000",()=>{
-    console.log("Server is running on port 3000")
-})
+const app = express();
+
+app.use(express.static(path.join(path.resolve(), "public")))
+app.use(express.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.post("/", (req, res) => {
+    console.log(req.body.name);
+    console.log(req.body.email);
+    console.log(req.body.no);
+    res.redirect("/");
+});
+
+app.listen("2000", () => {
+    console.log("Server is running on port 2000")
+});
