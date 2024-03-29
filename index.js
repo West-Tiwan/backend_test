822d177832ebe04bd1126c3a0b91bfe2826ece4c
West-Tiwan <xyzmohit.singh@gmail.com>
2024-03-29 22:50:39 +0530

    cookies

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
-const express = require("express");
-const path = require("path");
-const mongoose = require("mongoose");
-const {Collection, Schema} = mongoose;
+import express from "express";
+import path from "path";
+import pkg from 'mongoose';
+
+const {mongoose, Collection, Schema} = pkg;
+import cookieParser from "cookie-parser";
+
+
 const app = express();
 mongoose.connect("mongodb://localhost:27017", {dbName: "backend"}).then(console.log("DB connected")).catch((e) => {
     console.log("error occured : " + e)
-})
+});
 
 const formSchema = new mongoose.Schema({
     name: String,
     email: String,
     phone_no: Number,
-})
+});
 
-const reply = mongoose.Collection("Replies", formSchema)
+const reply = mongoose.model("Replies", formSchema);
 
-app.use(express.static(path.join(path.resolve(), "public")))
+app.use(express.static(path.join(path.resolve(), "public")));
 app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("index");
+    res.render("login");
 });
 
-app.post("/", (req, res) => {
-    console.log(req.body.name);
-    console.log(req.body.email);
-    console.log(req.body.number);
+app.post("/login", (req, res) => {
+    res.cookie("name", "yahoo", {
+        maxAge: new Date(new Date() + 1000 * 60),
+        httpOnly: true,
+        secure: true,
+    });
     res.redirect("/");
 });
 
