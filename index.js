56a7258de4bc983531e88b9e4fdd3b319e6459cd
West-Tiwan <xyzmohit.singh@gmail.com>
2024-03-31 01:53:45 +0530

    auth with JWT done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import pkg from 'mongoose';
+import jwt from "jsonwebtoken";
 
 const {mongoose, Collection, Schema} = pkg;
 import cookieParser from "cookie-parser";
@@ -11,13 +12,12 @@ mongoose.connect("mongodb://localhost:27017", {dbName: "backend"}).then(console.
     console.log("error occured : " + e)
 });
 
-const formSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     name: String,
     email: String,
-    phone_no: Number,
 });
 
-const reply = mongoose.model("Replies", formSchema);
+const user = mongoose.model("users", userSchema);
 
 app.use(express.static(path.join(path.resolve(), "public")));
 app.use(express.urlencoded({extended: true}));
@@ -25,16 +25,41 @@ app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
+const isAuthenticated = async (req, res, next) => {
+    const {token} = req.cookies;
+    if (token) {
+        const decoded = jwt.verify(token, "oi oi oi");
+        req.user = await user.findById(decoded._id);
+        next();
+    } else {
+        res.redirect("/login");
+    }
+};
+
+app.get("/", isAuthenticated, (req, res) => {
+    res.render("logout");
+});
+
+app.get("/login", (req, res) => {
     res.render("login");
 });
 
-app.post("/login", (req, res) => {
-    res.cookie("name", "yahoo", {
-        maxAge: new Date(new Date() + 1000 * 60),
-        httpOnly: true,
-        secure: true,
+app.post("/login", async (req, res) => {
+    const {name, email} = req.body;
+    const userReturn = await user.create({
+        name, email
     });
+    const token = jwt.sign({_id: userReturn._id}, "oi oi oi");
+    res.cookie("token", token, {expires: new Date(Date.now() + 60 * 1000), httpOnly: true});
+    res.redirect("/logout");
+});
+
+app.get("/logout", isAuthenticated, (req, res) => {
+    res.render("logout");
+});
+
+app.post("/logout", (req, res) => {
+    res.cookie("token", null, {expires: new Date(Date.now())});
     res.redirect("/");
 });
 
