d65c48a24c00f2598541720aaaf1e463a1de3be3
West-Tiwan <xyzmohit.singh@gmail.com>
2024-04-01 19:52:17 +0530

    find all user api

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017", {
+    dbName: "backend",
+});
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+});
+
+const User = mongoose.model("user", userSchema);
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.get("/users/all", async (req, res) => {
+    const users = await User.find();
+    res.json({
+        success: true,
+        users
+    })
+    res.end();
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
