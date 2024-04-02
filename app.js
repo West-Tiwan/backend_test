d78ae3814c86a0db79f6ba0dd038189b94fe6066
West-Tiwan <xyzmohit.singh@gmail.com>
2024-04-02 20:22:43 +0530

    route spliting

--- app.js
+++ app.js
@@ -1,59 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
+import userRouter from "./routes/user.js";
 
 const app = express();
 
+app.use(userRouter);
 app.use(express.json());
 
 mongoose.connect("mongodb://localhost:27017", {
     dbName: "backend",
-});
-
-const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-});
-
-const User = mongoose.model("user", userSchema);
-
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
-
-app.get("/user/reg", async (req, res) => {
-    // const users = await User.find();
-
-    const {name, email} = req.body;
-
-    const createdItem = await User.create({
-        name, email
-    });
-
-    res.json({
-        success: true,
-        message: "User created successfully",
-        data: createdItem,
-    });
-});
-
-app.get("/user/:id", async (req, res) => {
-    const {id} = req.params;
-
-    const user = await User.findById(id);
-
-    res.json({
-        success: true,
-        data: user,
-    });
-});
-
-app.get("/user/all", async (req, res) => {
-    const users = await User.find();
-
-    res.json({
-        success: true,
-        data: users,
-    });
+}).then(r => {
+    console.log("Connected to MongoDB")
 });
 
 app.listen(3000, () => {
---
+++ routes/user.js
@@ -0,0 +1,45 @@
+import express from "express";
+import {User} from "../models/user.js";
+
+const router = express.Router();
+router.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+router.get("/user/reg", async (req, res) => {
+    // const users = await User.find();
+
+    const {name, email} = req.body;
+
+    const createdItem = await User.create({
+        name, email
+    });
+
+    res.json({
+        success: true,
+        message: "User created successfully",
+        data: createdItem,
+    });
+});
+
+router.get("/user/:id", async (req, res) => {
+    const {id} = req.params;
+
+    const user = await User.findById(id);
+
+    res.json({
+        success: true,
+        data: user,
+    });
+});
+
+router.get("/user/all", async (req, res) => {
+    const users = await User.find();
+
+    res.json({
+        success: true,
+        data: users,
+    });
+});
+
+export default router;
